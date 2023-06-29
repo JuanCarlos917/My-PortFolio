e7cb17b0228501d6f04729732b97e426646339cd
Juan Gomez <juancarlos.gm931@gmail.com>
2023-06-29 09:23:44 -0500

    Se configura backend

---
+++ frontEnd/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+
+const NavBar = () => {
+    return (
+		<div className='bg-green-emerald flex items-center justify-end px-4'>
+			<p className='leading-normal text-3xl md:text-base font-light text-[#1a1919] font-rubik px-4'>
+				acerca de mi
+			</p>
+			<p className='leading-normal text-3xl md:text-base font-light text-[#1a1919] font-rubik px-4'>
+				proyectos
+			</p>
+			<p className='leading-normal text-3xl md:text-base font-light text-[#1a1919] font-rubik px-4'>
+				contacto
+			</p>
+		</div>
+	);
+    }
+    export default NavBar;
