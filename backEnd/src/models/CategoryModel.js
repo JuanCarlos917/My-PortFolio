e3b75f7e8f337e5d3c95717434a53ad17a7d74f3
Juan Gomez <juancarlos.gm931@gmail.com>
2023-07-07 09:43:16 -0500

    backEnd/src/category.  Crea
    se desarrolla controller GET. PUT y DELETE de category

--- backEnd/src/models/CategoryModel.js
+++ backEnd/src/models/CategoryModel.js
@@ -2,15 +2,21 @@ const { DataTypes } = require('sequelize');
 
 //Este modelo representara las categorías a las que pertenecen los proyectos. Se puedes asignar una o varias categorías a cada proyecto para organizarlos de acuerdo a su temática o tipo.
 module.exports = (sequelize) => {
-	sequelize.define('Category', {
-		id: {
-			type: DataTypes.INTEGER,
-			primaryKey: true,
-			autoIncrement: true,
+	sequelize.define(
+		'Category',
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			name: {
+				type: DataTypes.TEXT,
+				allowNull: false,
+			},
 		},
-		name: {
-			type: DataTypes.TEXT,
-			allowNull: false,
+		{
+			timestamps: false,
 		},
-	});
+	);
 };
