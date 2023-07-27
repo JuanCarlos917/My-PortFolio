099d95d6f628c31054eff45a7d034be78c0e565d
Juan Gomez <juancarlos.gm931@gmail.com>
2023-07-27 11:39:18 -0500

    backend/. Docker en proceso.

--- backEnd/index.js
+++ backEnd/index.js
@@ -2,7 +2,7 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(3001, () => {
     console.log('Server is listening at port 3001');
   });
