9ed93961cafda6a9c3439c31f77f1465254b7184
Juan Gomez <juancarlos.gm931@gmail.com>
2023-09-06 21:55:23 -0500

    frontEnd/src/dashboardAdmin/education   Se da estilo

--- frontEnd/src/dashboardAdmin/education/DeleteEducation.jsx
+++ frontEnd/src/dashboardAdmin/education/DeleteEducation.jsx
@@ -13,7 +13,6 @@ export const DeleteEducation = () => {
 	const educationInfo = useSelector(
 		(state) => state.education?.educationInfo,
 	);
-	// Busca la educación específica basándote en el ID
 	const specificEducation = Array.isArray(educationInfo)
 		? educationInfo.find((edu) => edu.id.toString() === id)
 		: null;
@@ -29,50 +28,82 @@ export const DeleteEducation = () => {
 	}, [dispatch, educationInfo]);
 
 	return (
-		<div>
+		<div className='container mx-auto p-4'>
 			{modified ? (
-				<div>¡Eliminación realizada con éxito!</div>
+				<div className='text-green-500 font-semibold text-center text-xl'>
+					¡Eliminación realizada con éxito!
+				</div>
 			) : (
 				<>
-					{status === 'loading' && <div>Eliminando...</div>}
-					{status === 'failed' && <div>{error}</div>}
+					{status === 'loading' && (
+						<div className='text-center font-semibold text-xl'>
+							Eliminando...
+						</div>
+					)}
+					{status === 'failed' && (
+						<div className='text-red-500 font-semibold text-center text-xl'>
+							{error}
+						</div>
+					)}
 
 					{!educationInfo ? (
-						<div>
-							<p>
-								No existe información de educación, para
+						<div className='text-center'>
+							<p className='mb-4'>
+								No existe información de educación para
 								eliminar.
 							</p>
-							<Link to='/dashboard/form-education'>Crear</Link>
+							<Link
+								to='/dashboard/form-education'
+								className='bg-green-500 text-white py-2 px-4 rounded'>
+								Crear
+							</Link>
 						</div>
 					) : (
-						<div>
-							<h4>Carrera:</h4>
-							<p>{specificEducation?.degree}</p>
-							<h4>Descripción:</h4>
-							<p>{specificEducation?.description}</p>
-							<h4>Universidad:</h4>
-							<p>{specificEducation?.institution}</p>
-							<h4>Campo de estudio:</h4>
-							<p>{specificEducation?.field_of_study}</p>
-							<h4>Fecha de inicio:</h4>
-							<p>{specificEducation?.startDate}</p>
-							<h4>Fecha de finalización:</h4>
-							<p>{specificEducation?.endDate}</p>
-							<button
-								onClick={() => {
-									dispatch(deleteEducation(id));
-								}}>
-								Eliminar
-							</button>
-							<Link to={`/dashboard/update-education/${id}`}>
-								Modificar
-							</Link>
+						<div className='p-4 border rounded'>
+							<div className='grid grid-cols-2'>
+								<h4 className='text-lg font-semibold'>
+									Grado:
+								</h4>
+								<p>{specificEducation?.degree}</p>
+								<h4 className='text-lg font-semibold'>
+									Descripción:
+								</h4>
+								<p>{specificEducation?.description}</p>
+								<h4 className='text-lg font-semibold'>
+									Universidad:
+								</h4>
+								<p>{specificEducation?.institution}</p>
+								<h4 className='text-lg font-semibold'>
+									Campo de estudio:
+								</h4>
+								<p>{specificEducation?.field_of_study}</p>
+								<h4 className='text-lg font-semibold'>
+									Fecha de inicio:
+								</h4>
+								<p>{specificEducation?.startDate}</p>
+								<h4 className='text-lg font-semibold'>
+									Fecha de finalización:
+								</h4>
+								<p>{specificEducation?.endDate}</p>
+							</div>
+							<div className='flex space-x-4 mt-4'>
+								<button
+									onClick={() => {
+										dispatch(deleteEducation(id));
+									}}
+									className='bg-red-500 text-white py-2 px-4 rounded'>
+									Eliminar
+								</button>
+								<Link
+									to={`/dashboard/update-education/${id}`}
+									className='bg-blue-500 text-white py-2 px-4 rounded'>
+									Modificar
+								</Link>
+							</div>
 						</div>
 					)}
 				</>
 			)}
 		</div>
 	);
-
 };
