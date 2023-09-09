31a0c003e796808a517dc9b4b54ee477ab16b9b6
Juan Gomez <juancarlos.gm931@gmail.com>
2023-09-08 23:22:22 -0500

    frontend/src/components/Loading. Cambio de estilo

--- frontEnd/src/components/Loading/Loading.jsx
+++ frontEnd/src/components/Loading/Loading.jsx
@@ -1,19 +1,7 @@
 import styles from './loading.module.css';
 export const Loading = () => {
 	return (
-		<div className={styles['lds-spinner']}>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+		<div className={styles['loader']}>
 		</div>
 	);
 };
