51ec442d109e30acb2e6a04df8f84b8bfb6bd593
Juan Gomez <juancarlos.gm931@gmail.com>
2023-09-19 17:58:08 -0500

    frontend/src/components/Home.jsx. se da estilo

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -4,6 +4,7 @@ import { getAbout } from '../features/about/aboutSlice';
 import { Loading } from './Loading/Loading';
 import { ContactMe } from './contactMe';
 
+
 export const Home = () => {
 	const dispatch = useDispatch();
 	const aboutInfo = useSelector((state) => state.about.aboutInfo);
@@ -29,33 +30,37 @@ export const Home = () => {
 					{error}
 				</div>
 			) : (
-				<div className='grid grid-cols-1 md:grid-cols-2 gap-8 items-start'>
-					<div className='space-y-2'>
-						<h1 className='text-4xl font-sf'>{`${
-							cvInfo?.name || 'Juan Carlos'
-						} ${cvInfo?.lastName || 'Gómez'}`}</h1>
-						<p className='text-lg font-ysabeau'>{aboutInfo?.bio}</p>
-						<div className='flex space-x-4 mt-4'>
-							<a
-								href={cvInfo?.social_media?.linkedin}
-								className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>
-								LinkedIn
-							</a>
-							<a
-								href={cvInfo?.social_media?.github}
-								className='bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded'>
-								GitHub
-							</a>
+				<div>
+					<div className='flex flex-col md:flex-row gap-8 items-start mb-8'>
+						<div className='flex-1 space-y-4'>
+							<h1 className='text-4xl font-sf'>{`${
+								cvInfo?.name || 'Juan Carlos'
+							} ${cvInfo?.lastName || 'Gómez'}`}</h1>
+							<p className='text-lg font-ysabeau'>
+								{aboutInfo?.bio}
+							</p>
+							<div className='flex space-x-4 mt-4'>
+								<a
+									href={cvInfo?.social_media?.linkedin}
+									className='bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded'>
+									LinkedIn
+								</a>
+								<a
+									href={cvInfo?.social_media?.github}
+									className='bg-black hover:bg-gray-800 text-white font-bold py-2 px-4 rounded'>
+									GitHub
+								</a>
+							</div>
+						</div>
+						<div className='flex-1'>
+							<img
+								src={cvInfo?.imageUrl}
+								alt='Juan Carlos Gomez'
+								className='w-full md:w-3/4 mx-auto'
+							/>
 						</div>
 					</div>
-					<div>
-						<img
-							src={cvInfo?.imageUrl}
-							alt='Juan Carlos Gomez'
-							className='rounded-full'
-						/>
-					</div>
-					<div>
+					<div className='text-center mt-8'>
 						<ContactMe />
 					</div>
 				</div>
