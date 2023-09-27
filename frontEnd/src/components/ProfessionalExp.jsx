414fceb05906ff22b84d63adf84ba3b01cd16c5c
Juan Gomez <juancarlos.gm931@gmail.com>
2023-09-27 16:41:45 -0500

    forntend/src/components/ Responsivo cv, education, porfesionalExp, project

--- frontEnd/src/components/ProfessionalExp.jsx
+++ frontEnd/src/components/ProfessionalExp.jsx
@@ -26,33 +26,29 @@ export const ProfessionalExp = () => {
 		);
 	} else if (status === 'succeeded') {
 		content = (
-			<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto'>
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-8 max-w-6xl mx-auto'>
 				{Array.isArray(professionalExpInfo) &&
 					professionalExpInfo.map((experience, index) => (
 						<div
 							key={index}
-							className='rounded-lg overflow-hidden shadow-lg p-6 bg-white'>
-							<h2 className='text-xl font-sf mt-4 mb-2'>
+							className='rounded-lg overflow-hidden shadow-lg p-4 sm:p-6 bg-white'>
+							<h2 className='text-lg sm:text-xl font-sf mb-2'>
 								{experience.company}
 							</h2>
-							<p className='text-gray-600 mb-4 font-ysabeau '>
+							<p className='text-gray-600 mb-3 font-ysabeau'>
 								<span className='font-sf '>Descripción:</span>{' '}
 								{experience.description}
 							</p>
-							<p className='text-gray-600 mb-4 font-ysabeau'>
+							<p className='text-gray-600 mb-3 font-ysabeau'>
 								<span className='font-sf'>Posición:</span>{' '}
 								{experience.position}
 							</p>
-							<p className='text-gray-600 mb-4 font-ysabeau'>
-								<span className='font-sf '>
-									Inicio:
-								</span>{' '}
+							<p className='text-gray-600 mb-3 font-ysabeau'>
+								<span className='font-sf '>Inicio:</span>{' '}
 								{experience.startDate}
 							</p>
 							<p className='text-gray-600 font-ysabeau'>
-								<span className='font-sf '>
-									Fin:
-								</span>{' '}
+								<span className='font-sf '>Fin:</span>{' '}
 								{experience.endDate}
 							</p>
 						</div>
@@ -63,5 +59,5 @@ export const ProfessionalExp = () => {
 		content = <div className='text-red-600 font-semibold'>{error}</div>;
 	}
 
-	return <div className='container mx-auto p-8'>{content}</div>;
+	return <div className='container mx-auto p-4 sm:p-8'>{content}</div>;
 };
