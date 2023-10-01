42ea8898292e61a65a6ed80777f68a951dc6e54f
Juan Gomez <juancarlos.gm931@gmail.com>
2023-09-30 22:47:06 -0500

    frontend/src/components/ProjetCard y ServiceCard Agregar Titulo h3

--- frontEnd/src/components/ProjectCard.jsx
+++ frontEnd/src/components/ProjectCard.jsx
@@ -28,40 +28,46 @@ export const ProjectCard = () => {
 		);
 	} else if (status === 'succeeded') {
 		content = (
-			<div className='grid grid-cols-2 gap-4'>
-				{projectInfo.map((project) => (
-					<Card
-						className='py-4 my-2 hover:shadow-goldenShadow'
-						key={project.id}>
-						<CardHeader className='pb-0 pt-2 px-4 flex-col items-start'>
-							<p className='text-tiny uppercase font-bold'>
-								{project.title}
-							</p>
-							<small className='text-default-500'>
-								{project.description}
-							</small>
-							<Link to={`/projects`}>
-								<h4 className='font-bold text-vivid_blue hover:text-soft_green'>
-									Ver más detalles
-								</h4>
-							</Link>
-						</CardHeader>
-						<CardBody className='overflow-visible py-2'>
-							<Image
-								alt={project.title}
-								className='object-cover rounded-xl'
-								src={project.image}
-								width={270}
-							/>
-							<Link to={project.url}>
-								<h4 className='font-bold text-vivid_blue hover:text-bright_red'>
-									Ver Proyecto
-								</h4>
-							</Link>
-						</CardBody>
-					</Card>
-				))}
-			</div>
+			<>
+				<Typography variant='h3' className='pb-3'>
+					Proyectos Destacados
+				</Typography>
+
+				<div className='grid grid-cols-2 gap-4'>
+					{projectInfo.map((project) => (
+						<Card
+							className='py-4 my-2 hover:shadow-goldenShadow'
+							key={project.id}>
+							<CardHeader className='pb-0 pt-2 px-4 flex-col items-start'>
+								<p className='text-tiny uppercase font-bold'>
+									{project.title}
+								</p>
+								<small className='text-default-500'>
+									{project.description}
+								</small>
+								<Link to={`/projects`}>
+									<h4 className='font-bold text-vivid_blue hover:text-soft_green'>
+										Ver más detalles
+									</h4>
+								</Link>
+							</CardHeader>
+							<CardBody className='overflow-visible py-2'>
+								<Image
+									alt={project.title}
+									className='object-cover rounded-xl'
+									src={project.image}
+									width={270}
+								/>
+								<Link to={project.url}>
+									<h4 className='font-bold text-vivid_blue hover:text-bright_red'>
+										Ver Proyecto
+									</h4>
+								</Link>
+							</CardBody>
+						</Card>
+					))}
+				</div>
+			</>
 		);
 	} else if (status === 'failed') {
 		content = <Typography color='error'>{error}</Typography>;
