3d6ea5e20f236cd545462471589244f90f44ffb3
Juan Gomez <juancarlos.gm931@gmail.com>
2023-10-03 12:49:16 -0500

    backend/src/controller, route,utils/ new feature getProjectById

--- backEnd/src/routes/projectRoutes.js
+++ backEnd/src/routes/projectRoutes.js
@@ -5,15 +5,23 @@ const {
 	createProject,
 	updateProject,
 	deleteProject,
+	getProjectById,
 } = require('../controllers/projectController');
+
+const {
+	projectValidations,
+	handleValidationErrors,
+} = require('../utils/projectValidations');
 
 // Ruta para obtener la información de proyectos
 router.get('/', getProjects);
 // Rutas para crear y actualizar el contenido de proyectos
-router.post('/', createProject);
-router.put('/:id', updateProject);
+router.post('/', projectValidations, handleValidationErrors, createProject);
+router.put('/:id', projectValidations, handleValidationErrors, updateProject);
 // Ruta para eliminar la información de proyectos
 router.delete('/:id', deleteProject);
+// Ruta para obtener la información de un proyecto
+router.get('/:id', getProjectById);
 
 
 module.exports = router;
