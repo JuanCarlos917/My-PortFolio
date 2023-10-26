01a91b5179dc857006c61fede656ee456b7c1046
Juan Gomez <juancarlos.gm931@gmail.com>
2023-10-26 18:06:31 -0500

    frontend/ Experiences feature actual

--- frontEnd/src/utils/FormValidationsExp.js
+++ frontEnd/src/utils/FormValidationsExp.js
@@ -7,8 +7,8 @@ export const FormValidationsExp = Yup.object().shape({
 	startYearMonth: Yup.string()
 		.matches(/^\d{4}-\d{2}$/, 'El formato debe ser YYYY-MM')
 		.required('El mes y año de inicio son requeridos'),
-	endYearMonth: Yup.string()
-		.matches(/^\d{4}-\d{2}$/, 'El formato debe ser a YYYY-MM')
+	// endYearMonth: Yup.string()
+	// 	.matches(/^\d{4}-\d{2}$/, 'El formato debe ser a YYYY-MM')
 });
 
 export default FormValidationsExp;
